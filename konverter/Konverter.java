a0f846f1b99e2375f060600a9cafa2e41a8a3483
Ljubisa Nesic <ljubisanesicdo@gmail.com>
2018-08-05 23:51:53 +0200

    pocetna verzija - kostur

---
+++ konverter/Konverter.java
@@ -0,0 +1,45 @@
+package konverter;
+/*
+ * Napisati i testirati jednostavnu „univerzalni“ konverter klasu. 
+ * Klasa treba da podržava sljedeće konverzije:
+
+a.      Iz kilograma u funte
+b.      Iz funti u kilograme
+c.      Iz centimetara u inče
+d.      Iz inča u centimetre
+e.      Iz celzijusa u farenhajte
+f.     Iz farenhajti u celzijuse
+
+* Testirati klasu sa unosom od strane korisnika.
+* Korisnik ima izbor konverzije + mogućnost unosa. 
+*/
+public class Konverter {
+	
+	private Konverter() {
+		
+	}
+	
+	public static void izKilogramaUFunte(double vrijednost) {
+		
+	}
+
+	public static void izFuntiUKilograme(double vrijednost) {
+		
+	}
+	
+	public static void izCentimetaraUInche(double vrijednost) {
+		
+	}
+	
+	public static void izInchaUCentrimetre(double vrijednost) {
+		
+	}
+	
+	public static void izCelzijusaUFerenhajte(double vrijednost) {
+		
+	}
+	
+	public static void izFerenhajtaUCelzijuse(double vrijednost) {
+		
+	}
+}
